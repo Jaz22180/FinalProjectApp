84337fe850af3bb9d068194d24fdfc2c75fb3084
Jazmin Guiza <guizajazmin2@gmail.com>
2025-11-17 02:57:41 -0600

    Refactor App class and add references handling
    Se añade modificación para referencia APA7 INCOMPLETO

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from customtkinter import *
-from tkinter import * 
 from tkinter import filedialog
 import numpy as np
 import tkinter.messagebox
@@ -8,6 +6,8 @@ from tkinter import *
 from tkinter import filedialog
 import numpy as np
 import tkinter.messagebox
+import sys                                                          #NUEVO JAZ
+sys.stdout.reconfigure(encoding='utf-8')                            #NUEVO JAZ
 
 class App:
     
@@ -34,6 +34,9 @@ class App:
         # Etiquetas
         self.nombre_archivo_label = CTkLabel(master, text = "Nombre del archivo: ", anchor = 'w', width = self.ancho/3, height = self.alto/14)
         self.nombre_archivo_label.place(x = self.ancho/12, y = self.alto/12 * 2)
+        self.referencias = CTkLabel(master, text = "Referencias: ", anchor = 'w', width = self.ancho/3, height = self.alto/19) #NUEVO JAZ
+        self.referencias.place(x = self.ancho/12, y = self.alto/12 * 3)                                                        #NUEVO JAZ
+
         # Nombre de los desarroladores     
         self.dev_label = CTkLabel(master, text = "Developers: Jazmín, Alexis & Emiliano", anchor = 'w', width = self.ancho/6, height = self.alto/25)  
         self.dev_label.place(x = self.ancho / 8 * 6, y = self.alto / 35 * 28) 
@@ -46,7 +49,7 @@ class App:
             
             self.cont = 0
             # Abrir la ruta del archivo
-            self.filename = filedialog.askopenfilename(initialdir='C:/', title = "Abrir archivo para procesar sus textos", filetypes = (("txt files", "*.txt"), ))
+            self.filename = filedialog.askopenfilename(initialdir='C:/', title = "Abrir archivo para procesar sus textos")
             # Inicializar los renglones del archivo
             self.filas = []
             
@@ -57,10 +60,10 @@ class App:
                 self.nombre_archivo_label.configure(text = "Nombre del archivo: " + str(self.file_name_t), anchor = "w")
                 self.nombre_header = "PROTOCOLO DE INVESTIGACIÓN"
                 # "Barrer" el archivo línea por línea
-                with open(self.filename, "r", encoding='utf-8') as archivo:
+                with open(self.filename, "r", encoding="utf-8-sig", errors="replace") as archivo:
                     for linea in archivo:
                         # linea.isspace(): la línea contiene puros espacios en blanco
-                        if linea != "" and linea.isspace() == False and self.eliminarHeader(self.nombre_header, linea):
+                        if linea != "" and linea.isspace() == False and self.eliminarHeader(self.nombre_header, linea): 
                             # rstrip() elimina el salto de línea al final
                             self.filas.append(linea.rstrip())
                             # Imprimir en consola la fila "cont" 
@@ -73,11 +76,14 @@ class App:
                 
                 tkinter.messagebox.showinfo(title = "Aviso", message="Archivo Cargado")
             print(str(self.filename))
+            self.ReferenciasAPA7()                                          #NUEVO JAZ
 
         except FileNotFoundError:
             tkinter.messagebox.showinfo(title = "Error", message="El archivo no existe en la ruta especificada")
-        except:
-            tkinter.messagebox.showinfo(title = "Error", message="Un error ocurrió al momento de cargar el archivo")
+
+        except Exception as e:
+            print("ERROR REAL:", e)                                                                                      #NUEVO JAZ
+            tkinter.messagebox.showinfo(title="Error", message="Un error ocurrió al momento de cargar el archivo")
 
     # Método para eliminar el header que tiene una imagen 
     def eliminarHeader(self, text_b, lin):
@@ -95,72 +101,38 @@ class App:
         else:
             return True
 
-GUI = CTk( )
-AppTXTs = App(GUI)
-AppTXTs.master.mainloop()
-class App:
+    #Método para comprobar si las referencias están en formato APA7           #NUEVO JAZ
+    def ReferenciasAPA7(self):
+        #ENCONTRANDO SECCIÓN DE REFERENCIA
+        fin = None
+        for i, fila in enumerate(self.filas):
+            if "REFERENCIAS" in fila:
+               fin = i
+               break
+
+        referencias_encontradas = self.filas[fin + 1:]
+        referencias_separadas = []   # lista final de referencias ya separadas
+        referencia_actual = []       # acumula una referencia
+
+        for fila in referencias_encontradas:
+            referencia_actual.append(fila)
+
+        # Cuando aparece DOI se cierra una referencia
+        if "DOI" in fila.upper():
+            referencias_separadas.append(referencia_actual)
+            referencia_actual = []        # reiniciar para la siguiente referencia
+
     
-    # Constructor
-    def __init__(self, master):
-        
-        # Atributos/Características de la clase
-        # Listas de frames del GIF de inicio (splash screen)
+        if referencia_actual:
+            referencias_separadas.append(referencia_actual)
 
-        self.state_stop = False
-        self.master = master
-        self.master.title("Análisis bibliográficos de textos")
-        self.master.state("zoomed")
-        self.master.resizable(0, 0) 
-        self.ancho = master.winfo_screenwidth()
-        self.alto = master.winfo_screenheight()
-        self.file_name_t = ""
+    # Convertir a texto para mostrar
+        texto = ""
+        for ref in referencias_separadas:
+            texto += "\n".join(ref) + "\n\n"
 
-        # Botones
-        self.txts_btn = CTkButton(master, text = "Selecciona la ruta del archivo a examinar", command = self.elegirPathTXTs, width = self.ancho/3, height = self.alto/12)
-        self.txts_btn.place(x = self.ancho/12, y = self.alto/12)
+        self.referencias.configure(text=texto, anchor="w")
 
-        # Etiquetas
-        self.nombre_archivo_label = CTkLabel(master, text = "Nombre del archivo: ", anchor = 'w', width = self.ancho/3, height = self.alto/14)
-        self.nombre_archivo_label.place(x = self.ancho/12, y = self.alto/12 * 2)
-        # Nombre de los desarroladores     
-        self.dev_label = CTkLabel(master, text = "Developers: Jazmín, Alexis & Emiliano", anchor = 'w', width = self.ancho/6, height = self.alto/25)  
-        self.dev_label.place(x = self.ancho / 8 * 6, y = self.alto / 35 * 28) 
-
-    # Métodos
-    def elegirPathTXTs(self):
-        
-        # Try and except para cachar los errores al cargar el achivo
-        try:
-            
-            cont = 0
-            # Abrir la ruta del archivo
-            self.filename = filedialog.askopenfilename(initialdir='C:/', title = "Abrir archivo para procesar sus textos", filetypes = (("txt files", "*.txt"), ))
-            # Inicializar los renglones del archivo
-            self.filas = []
-            
-            if self.filename != "":
-                # Obtener solo el nombre del archivo sin todo el path
-                self.file_name_t = os.path.basename(self.filename)
-                # Setear el texto de la etiqueta para que incluya el nombre del archivo
-                self.nombre_archivo_label.configure(text = "Nombre del archivo: " + str(self.file_name_t), anchor = "w")
-                # "Barrer" el archivo línea por línea
-                with open(self.filename, "r", encoding='utf-8') as archivo:
-                    for linea in archivo:
-                        # linea.isspace(): la línea contiene puros espacios en blanco
-                        if linea != "" and linea.isspace() == False:
-                            # rstrip() elimina el salto de línea al final
-                            self.filas.append(linea.rstrip())
-                            # Imprimir en consola la fila "cont"
-                            print(self.filas[cont])  
-                            cont += 1
-            
-            tkinter.messagebox.showinfo(title = "Aviso", message="Archivo Cargado")
-            print(str(self.filename))
-
-        except FileNotFoundError:
-            tkinter.messagebox.showinfo(title = "Error", message="El archivo no existe en la ruta especificada")
-        except:
-            tkinter.messagebox.showinfo(title = "Error", message="Un error ocurrió al momento de cargar el archivo")
 
 
 GUI = CTk( )
